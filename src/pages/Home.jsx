aafcb86a13dd874e25479f313cae217bbaba27f7
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-17 10:46:10 -0700

    add logo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import hockey_logo from "../assets/nan-hockey-logo.png"
+
+export default function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      <img src={hockey_logo} alt="Nanaimo Adult Hockey League" className="logo" />
+    </div>
+    
+  )
+}
