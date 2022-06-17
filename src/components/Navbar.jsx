7d548d1416ceba48c874121e5753626f379db5b5
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-17 12:30:00 -0700

    add img array to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,12 @@ export default function Navbar() {
   return (
     <div>
       <nav className="main">
-        <img src={hockey_logo} alt="Nanaimo Adult Hockey League" className="hockey-logo" />
+        
         <div className="navigation">
           <div>NEWS</div>
           <div>DIVISIONS</div>
           <div>SPARE BOARD & REGISTRATION</div>
+          <img src={hockey_logo} alt="Nanaimo Adult Hockey League" className="hockey-logo" />
           <div>LEAGUE & SOCIETY</div>
           <div>REFEREES SECTION</div>
           <div>SUSPENSIONS</div>
