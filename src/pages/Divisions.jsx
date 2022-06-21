a347625ce535016a23745ac3b767f75e46015848
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-21 13:13:03 -0700

    style table and division cards

--- src/pages/Divisions.jsx
+++ src/pages/Divisions.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 // images
 import hockey_logo from "../assets/nan-hockey-logo.png"
+import landlubber from "../assets/landlubber.png"
+import longwood from "../assets/longwood.png"
+import quarterway from "../assets/Quarterway.png"
+import white_sails from "../assets/white-sails.png"
+
 
 // styles
 import './Divisions.css'
@@ -9,76 +14,109 @@ import './Divisions.css'
 export default function Divisions() {
   return (
     <div>
-      <div className="table-container">     
-      <table >            
-        <caption>Team Representatives</caption>
-        <thead>
-          <tr>
-            <th>Team</th>
-            <th>Team Rep</th>
-            <th>Cell</th>
-            <th>Email</th>
-          </tr>        
-        </thead>    
-        <tbody>
-          <tr>
-            <th>REC A</th>        
-          </tr>
-          <tr>
-            <td>Canadiens</td>
-            <td>Mike Hrabowych</td>
-            <td>(250)714-6101</td>
-            <td>mike.hrabowych.com</td>
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-          <tr>
-            <th>REC B</th>        
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-          <tr>
-            <td>Crabbers</td>
-            <td>Cole Maberley</td>
-            <td>(517)888-7094</td>
-            <td>cmaberley@rocketmail.com</td>
-          </tr>
-        </tbody>
-      </table>
-      </div> 
-               
+      <div className="container-main">
+        <div className="division-container">
+          <div className="landlubber">
+            <img src={landlubber}></img>
+            <p>LANDLUBBER PUB DIVISION (REC)</p>
+          </div>
+          <div className="quarterway">
+            <img src={quarterway}></img>
+            <p>QUARTERWAY PUB DIVISION (30+)</p>
+          </div>
+          <div className="longwood">
+            <img src={longwood}></img>
+            <p>LONGWOOD BREW PUB DIVISION (45+)</p>
+          </div>
+          <div className="white-sails">
+            <img src={white_sails}></img>         
+            <p>WHITE SAILS BREWING DIVISION (55+)</p>
+          </div>
+        </div>
+        <div className="table-container">
+          <table>            
+            <caption>Team Representatives</caption>           
+              <tr>     
+              <table>                
+              <tr>
+                <th className="rec">REC A</th>        
+              </tr>
+              <tr>
+                <th>Team</th>
+                <th>Team Rep</th>
+                <th>Cell</th>
+                <th>Email</th>
+              </tr>
+              <tr>
+                <td>Canadiens</td>
+                <td>Mike Hrabowych</td>
+                <td>(250)714-6101</td>
+                <td>mike.hrabowych.com</td>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>              
+              </table>
+              </tr>
+
+              <tr>
+                <table>
+              <tr>
+                <th className="rec">REC B</th>        
+              </tr>
+              <tr>
+                <th>Team</th>
+                <th>Team Rep</th>
+                <th>Cell</th>
+                <th>Email</th>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>
+              <tr>
+                <td>Crabbers</td>
+                <td>Cole Maberley</td>
+                <td>(517)888-7094</td>
+                <td>cmaberley@rocketmail.com</td>
+              </tr>
+
+  </table>
+</tr>
+              
+            
+          </table>
+        </div>
+      </div>      
       
     </div>
   )
