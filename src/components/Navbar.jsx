7308d3a97aca1e617df8437e2ddabb1963d84f50
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-25 12:03:12 -0700

    update front page media query 500px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,8 +16,8 @@ export default function Navbar() {
   const { user } = useAuthContext()  
 
   const handleClick = (e) => {
-    const navbarLinks = document.querySelectorAll('.navigation > li');
-    navbarLinks.classList.toggle('active')
+    const navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active')
   }
 
   return (    
@@ -27,17 +27,18 @@ export default function Navbar() {
           <span className="bar"></span>
           <span className="bar"></span>
         </a>
-        <div className='navbar-links'>      
+        <div className='logo-container'>
+          <Link to="/"><img src={hockey_logo} alt="Nanaimo Adult Hockey League" className="hockey-logo" /></Link>      
+        </div>
           <ul className="navigation">
             <li>NEWS</li>
             <li><Link to="/divisions">DIVISIONS</Link></li>          
-            <Link to="/"><img src={hockey_logo} alt="Nanaimo Adult Hockey League" className="hockey-logo" /></Link>
-            <li>REGISTRATION</li>          
+            <li></li>          
+            <li>REGISTRATION</li>
             {/* {!user && <li><Link to="/signup">SIGNUP</Link></li>} */}
             {!user && <li><Link to="/login">LOGIN</Link></li>}
             {user && <li onClick={logout}>LOGOUT</li>}
-          </ul>
-        </div>       
+          </ul>      
       </nav>       
   )
 }
