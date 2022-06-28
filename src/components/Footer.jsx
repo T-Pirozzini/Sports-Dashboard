00d0f3197d0530a73641f8366b1d9b56136e9637
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-06-27 18:50:31 -0700

    add links to logout/login in footer and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 // styles
 import './Footer.css'
 
+// context
+import { useAuthContext } from '../hooks/useAuthContext'
+
 export default function Footer() {
+  const { logout } = useLogout()
+  const { user } = useAuthContext() 
+
   return (
     <div>
       {/* footer */}
       <div className="footer-main">
         <div className="footer-container">
           <ul className="about-footer">            
-            <li>NEWS</li>
-            <li>RULES/REGULATIONS</li>            
+            <li><Link to="/news">NEWS</Link></li>
+            <li><Link to="/standings">RULES/REGULATIONS</Link></li>            
           </ul>
           <ul className="divisions-footer">
-            <li>DIVISIONS</li>
-            <li>STANDINGS</li>            
+            <li><Link to="/divisions">DIVISIONS</Link></li>
+            <li><Link to="/standings">STANDINGS</Link></li>            
           </ul>
           <ul className="league-society">
-            <li>REFEREE</li>
-            <li>REPORT</li>                     
+            <li><Link to="/standings">REFEREE</Link></li>
+            <li><Link to="/report">REPORT</Link></li>                     
           </ul>
           <ul className="contribute">
-            <li>ADMIN LOGIN</li>                                
+            {!user && <li><Link to="/login">ADMIN LOGIN</Link></li>}        
+            {user && <li className='auth-links' onClick={logout}>LOGOUT</li>}                                 
           </ul>
         </div>
         <div className="mobile-footer-container">
@@ -33,8 +41,9 @@ export default function Footer() {
           <ul>
             <li><Link to="/divisions">DIVISIONS</Link></li>            
           </ul>          
-          <ul>            
-            <li><Link to="/report">LOGIN</Link></li>                    
+          <ul>
+            {!user && <li><Link to="/login"> ADMIN LOGIN</Link></li>}        
+            {user && <li onClick={logout}>LOGOUT</li>}                    
           </ul>
         </div>
       </div>
